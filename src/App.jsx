926f2844335557ca2cb1f3bb816f929d9ec0f369
ajummer <makeudreamempty@gmail.com>
2024-04-29 13:22:08 +0300

    Add bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <h1 className="text-xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="h-screen w-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#1f3756] to-[#141539]"></div>
+  );
 }
