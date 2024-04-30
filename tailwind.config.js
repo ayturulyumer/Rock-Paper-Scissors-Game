335b48d0304dcfd986d6920bd66b60f7e14c0763
ajummer <makeudreamempty@gmail.com>
2024-04-30 12:24:18 +0300

    Add screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {},
+  },
+  screens: {
+    phone: "640px",
+    tablet: "768px",
+    laptop: "1024px",
+    desktop: "1280px",
   },
   plugins: [require("daisyui")],
 };
