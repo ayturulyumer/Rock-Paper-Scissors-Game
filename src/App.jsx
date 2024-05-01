c51cb32b02eaca3bfc50591746f500f1c9a9409c
ajummer <makeudreamempty@gmail.com>
2024-05-01 11:50:41 +0300

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Header from "./components/Header/Header.jsx";
+
 export default function App() {
   return (
-    <div className="h-screen w-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#1f3756] to-[#141539]"></div>
+    <div className="h-screen w-screen flex justify-center font-body bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#1f3756] to-[#141539]">
+      <Header />
+    </div>
   );
 }
