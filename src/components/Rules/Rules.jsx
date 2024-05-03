6c81c280d8dc1732e99805e9bc4900cc32aadeb8
ajummer <makeudreamempty@gmail.com>
2024-05-03 10:34:43 +0300

    Add Rules component

---
+++ src/components/Rules/Rules.jsx
@@ -0,0 +1,7 @@
+export default function Rules() {
+  return (
+    <div className="h-10 w-32 mt-32 border-2 rounded-md flex justify-center items-center">
+      <p className="uppercase text-[#ffffff] tracking-widest cursor-pointer">Rules</p>
+    </div>
+  );
+}
