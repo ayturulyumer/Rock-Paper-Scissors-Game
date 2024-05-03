01bb470dd754f285035996d8e967028871c16f72
ajummer <makeudreamempty@gmail.com>
2024-05-03 10:43:34 +0300

    Add Rules and Game component s

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header/Header.jsx";
+import Game from "./components/Game/Game.jsx";
+import Rules from "./components/Rules/Rules.jsx";
 
 export default function App() {
   return (
-    <div className="h-screen w-screen flex justify-center font-body bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#1f3756] to-[#141539]">
+    <div className="h-screen w-screen flex flex-col items-center gap-24  font-body bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#1f3756] to-[#141539]">
       <Header />
+      <Game />
+      <Rules/>
     </div>
   );
 }
