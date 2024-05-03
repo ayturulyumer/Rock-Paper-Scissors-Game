1d717df1843a5fb1410ae5e9000a880c69ce02e6
ajummer <makeudreamempty@gmail.com>
2024-05-03 10:06:34 +0300

    Add cursor pointer

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -7,35 +7,35 @@ export default function Game() {
           src="./src/assets/images/bg-pentagon.svg"
           alt="pentagon"
         />
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#ec9e0e] absolute top-[-30px] left-[50%] rounded-full transform -translate-x-1/2">
+        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#ec9e0e] absolute top-[-30px] left-[50%] cursor-pointer rounded-full transform -translate-x-1/2 ">
           <img
             className="w-9 h-9"
             src="./src/assets/images/icon-scissors.svg"
             alt="scissor"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#4865f4] absolute top-[45px] left-[100%] rounded-full transform -translate-x-1/2">
+        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#4865f4] absolute top-[45px] left-[100%] cursor-pointer rounded-full transform -translate-x-1/2">
           <img
             className="w-9 h-9"
             src="./src/assets/images/icon-paper.svg"
             alt="scissor"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#40b9ce] absolute top-[50px]  rounded-full transform -translate-x-1/2">
+        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#40b9ce] absolute top-[50px] cursor-pointer  rounded-full transform -translate-x-1/2">
           <img
             className="w-9 h-9"
             src="./src/assets/images/icon-spock.svg"
             alt="scissor"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#834fe3] absolute bottom-[-40px] left-[20%]  rounded-full transform -translate-x-1/2">
+        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#834fe3] absolute bottom-[-40px] left-[20%] cursor-pointer  rounded-full transform -translate-x-1/2">
           <img
             className="w-9 h-9"
             src="./src/assets/images/icon-lizard.svg"
             alt="scissor"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#dc2e4e] absolute bottom-[-40px] left-[80%]  rounded-full transform -translate-x-1/2">
+        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#dc2e4e] absolute bottom-[-40px] left-[80%] cursor-pointer  rounded-full transform -translate-x-1/2">
           <img
             className="w-9 h-9"
             src="./src/assets/images/icon-rock.svg"
