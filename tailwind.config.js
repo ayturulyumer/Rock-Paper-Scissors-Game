969725853b2d658838d1dcc4710829a8f5d1c8f7
ajummer <makeudreamempty@gmail.com>
2024-05-04 11:54:26 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    colors: {},
+    colors: {
+      ...colors,
+    },
     fontFamily: {
       body: ["Barlow Semi Condensed"],
     },
