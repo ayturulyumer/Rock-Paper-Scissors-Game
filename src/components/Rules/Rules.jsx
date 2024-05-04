f8e6a04d49c9d6666264cae8dd6d9f0ccae5a29f
ajummer <makeudreamempty@gmail.com>
2024-05-04 11:59:50 +0300

    add onClick event to toggle modal

--- src/components/Rules/Rules.jsx
+++ src/components/Rules/Rules.jsx
@@ -1,7 +1,12 @@
-export default function Rules() {
+
+
+export default function Rules({toggleShowModal}) {
   return (
-    <button className="h-10 w-32 mt-32  mb-8 border-2 rounded-md flex justify-center items-center cursor-pointer tablet:mt-11 desktop:self-end desktop:mr-8 desktop:mt-0 ">
+    <button
+      onClick={toggleShowModal}
+      className="h-10 w-32 mt-32  mb-8 border-2 rounded-md flex justify-center items-center cursor-pointer tablet:mt-11 desktop:self-end desktop:mr-8 desktop:mt-0 "
+    >
       <p className="uppercase text-[#ffffff] tracking-widest">Rules</p>
     </button>
   );
-}   
+}
