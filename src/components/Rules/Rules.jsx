698a7626ebbef857c113c224398ecbf7b6eb09f3
ajummer <makeudreamempty@gmail.com>
2024-05-04 09:17:49 +0300

    change div to btn

--- src/components/Rules/Rules.jsx
+++ src/components/Rules/Rules.jsx
@@ -1,7 +1,7 @@
 export default function Rules() {
   return (
-    <div className="h-10 w-32 mt-32 border-2 rounded-md flex justify-center items-center tablet:mt-11">
-      <p className="uppercase text-[#ffffff] tracking-widest cursor-pointer">Rules</p>
-    </div>
+    <button className="h-10 w-32 mt-32  mb-8 border-2 rounded-md flex justify-center items-center cursor-pointer tablet:mt-11 desktop:self-end desktop:mr-8 desktop:mt-0 ">
+      <p className="uppercase text-[#ffffff] tracking-widest">Rules</p>
+    </button>
   );
-}
+}   
