b7dae8385cf187cec682743f61b2d3cd245a063a
ajummer <makeudreamempty@gmail.com>
2024-05-07 11:49:30 +0300

    add onclick events to divs

--- src/components/HandSelection/HandSelection.jsx
+++ src/components/HandSelection/HandSelection.jsx
@@ -1,4 +1,4 @@
-export default function HandSelection() {
+export default function HandSelection({ handleHandSelection }) {
   return (
     <main>
       <div className="relative">
@@ -7,35 +7,55 @@ export default function HandSelection() {
           src="./src/assets/images/bg-pentagon.svg"
           alt="pentagon"
         />
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#ec9e0e] absolute top-[-30px] left-[50%] cursor-pointer rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px] ">
+        <div
+          id="scissor"
+          className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#ec9e0e] absolute top-[-30px] left-[50%] cursor-pointer rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px] "
+          onClick={handleHandSelection}
+        >
           <img
             className="w-9 h-9 tablet:w-12 tablet:h-12 desktop:w-14 desktop:h-14"
             src="./src/assets/images/icon-scissors.svg"
             alt="scissor"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#4865f4] absolute top-[55px] left-[100%] cursor-pointer rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px] desktop:top-[80px]">
+        <div
+          id="paper"
+          className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#4865f4] absolute top-[55px] left-[100%] cursor-pointer rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px] desktop:top-[80px]"
+          onClick={handleHandSelection}
+        >
           <img
             className="w-9 h-9 tablet:w-12 tablet:h-12 desktop:w-14 desktop:h-14"
             src="./src/assets/images/icon-paper.svg"
             alt="paper"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#40b9ce] absolute top-[55px] cursor-pointer  rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px] desktop:top-[80px]">
+        <div
+          id="spock"
+          className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#40b9ce] absolute top-[55px] cursor-pointer  rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px] desktop:top-[80px]"
+          onClick={handleHandSelection}
+        >
           <img
             className="w-9 h-9 tablet:w-12 tablet:h-12 desktop:w-14 desktop:h-14"
             src="./src/assets/images/icon-spock.svg"
             alt="spock"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#834fe3] absolute bottom-[-40px] left-[20%] cursor-pointer  rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px]">
+        <div
+          id="lizard"
+          className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#834fe3] absolute bottom-[-40px] left-[20%] cursor-pointer  rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px]"
+          onClick={handleHandSelection}
+        >
           <img
             className="w-9 h-9 tablet:w-12 tablet:h-12 desktop:w-14 desktop:h-14"
             src="./src/assets/images/icon-lizard.svg"
             alt="lizard"
           />
         </div>
-        <div className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#dc2e4e] absolute bottom-[-40px] left-[80%] cursor-pointer  rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px]">
+        <div
+          id="rock"
+          className="w-24 h-24 flex items-center bg-[#ffffff] justify-center border-[12px] border-[#dc2e4e] absolute bottom-[-40px] left-[80%] cursor-pointer  rounded-full transform -translate-x-1/2 tablet:w-28 tablet:h-28 desktop:w-36 desktop:h-36 desktop:border-[16px]"
+          onClick={handleHandSelection}
+        >
           <img
             className="w-9 h-9 tablet:w-12 tablet:h-12 desktop:w-14 desktop:h-14"
             src="./src/assets/images/icon-rock.svg"
