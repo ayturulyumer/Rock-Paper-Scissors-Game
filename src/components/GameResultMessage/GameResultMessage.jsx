7f5fb6ef22b7b468fe995bbc3a93930136c3cbb7
ajummer <makeudreamempty@gmail.com>
2024-05-08 11:21:58 +0300

    Add result message

---
+++ src/components/GameResultMessage/GameResultMessage.jsx
@@ -0,0 +1,10 @@
+export default function GameResultMessage({ result }) {
+  return (
+    <div className="flex flex-col justify-center items-center gap-6">
+      <h1 className="uppercase text-center text-6xl text-white">{result}</h1>
+      <button className="h-12 w-56 bg-white border-2 rounded-md flex justify-center items-center cursor-pointer ">
+        <p className="uppercase tracking-widest text-[#3b4363]">Play Again</p>
+      </button>
+    </div>
+  );
+}
