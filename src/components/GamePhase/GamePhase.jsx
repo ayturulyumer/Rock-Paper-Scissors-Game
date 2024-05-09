901d7734f787204a3103d96e35db2fc0fe60bea1
ajummer <makeudreamempty@gmail.com>
2024-05-09 09:40:40 +0300

    Add seperate component for picked hand
    to add more code reusability

--- src/components/GamePhase/GamePhase.jsx
+++ src/components/GamePhase/GamePhase.jsx
@@ -1,6 +1,6 @@
-import getBorderColor from "../../utils/getBorderColor.js";
 import { useState, useEffect } from "react";
 import GameResultMessage from "../GameResultMessage/GameResultMessage.jsx";
+import GamePhasePickedHand from "../GamePhasePickedHand/GamePhasePickedHand.jsx";
 
 const hands = ["rock", "paper", "scissors", "lizard", "spock"];
 
@@ -66,43 +66,13 @@ export default function GamePhase({
 
   return (
     <section className="w-screen h-auto flex justify-center flex-wrap gap-20 ">
-      <div className="flex flex-col gap-4 items-center basis-1/4 tablet:basis-4/12 laptop:basis-5/12 desktop:basis-0">
-        <h1 className="uppercase text-white order-last desktop::order-first desktop:text-3xl desktop: mb-14">
-          You picked
-        </h1>
-        <button
-          className={`w-28 h-28 flex items-center bg-white justify-center border-[12px] border-[${getBorderColor(
-            playerHand
-          )}] rounded-full  shadow-gray  tablet:w-28 tablet:h-28 desktop:w-72 desktop:h-72 desktop:border-[35px]`}
-        >
-          <img
-            className="w-12 h-12 tablet:w-12 tablet:h-12 desktop:w-28 desktop:h-28"
-            src={`./src/assets/images/icon-${playerHand}.svg`}
-            alt={playerHand}
-          />
-        </button>
-      </div>
+      <GamePhasePickedHand message={"You Picked"} hand={playerHand} />
       {result && (
         <div className="hidden self-center desktop:block">
           <GameResultMessage result={result} playAgain={playAgain} />
         </div>
       )}
-      <div className="flex flex-col gap-4 items-center basis-1/4 tablet:basis-4/12 laptop:basis-5/12 desktop:basis-0">
-        <h1 className="uppercase text-white order-last desktop::order-first desktop:text-3xl desktop:mb-14">
-          The House Picked
-        </h1>
-        <button
-          className={`w-28 h-28 flex items-center bg-white justify-center border-[12px] border-[${getBorderColor(
-            computerHand
-          )}] rounded-full  shadow-gray  tablet:w-28 tablet:h-28 desktop:w-72 desktop:h-72 desktop:border-[35px]`}
-        >
-          <img
-            className="w-12 h-12 tablet:w-12 tablet:h-12 desktop:w-28 desktop:h-28"
-            src={`./src/assets/images/icon-${computerHand}.svg`}
-            alt={computerHand}
-          />
-        </button>
-      </div>
+      <GamePhasePickedHand message={"The House Picked"} hand={computerHand} />
       {result && (
         <div className="flex-wrap desktop:hidden">
           <GameResultMessage result={result} />
